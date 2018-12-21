67acebf3e9a793420901eea31e54af8091cc685c
ColeyG <cjgeerts@gmail.com>
2018-12-20 23:07:04 -0500

    asdf

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const client = new Discord.Client();
 var cron = require('node-cron');
 const axios = require('axios');
 
+let basdfeep;
+
 client.on('ready', () => {
     client.generateInvite(["ADMINISTRATOR"]).then(link =>{
         console.log('You can add a discord bot here: '+link);
